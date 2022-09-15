bf7aa35d07503434103feafa944476e5d69e4dc7
Nolan Clavereuil <nclavereuil@dombtsig.local>
2022-09-15 14:43:42 +0200

    slam4

---
+++ mini-mvc-sample-1.7/views/global/about.php
@@ -0,0 +1,12 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="card">
+            <div class="card-body">
+                <h3>A propos</h3>
+                <p>
+                    Ceci est une page à prposo.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
